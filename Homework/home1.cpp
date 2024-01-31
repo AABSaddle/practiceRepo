45ad210255dd59db16eabfc3afba3706495abbc1
Aiden Byrne <157773076+AABSaddle@users.noreply.github.com>
2024-01-31 03:09:57 +0000

    commit

---
+++ Homework/home1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Hello! I can repeat the numbers you type! Try it: ";
+    cin >> num;
+    cout << num << endl;
+}
---
+++ Homework/home2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    cout << "Hello! I can add the numbers you type! Try it: ";
+    cin >> num1;
+    cout << "And the second: ";
+    cin >> num2;
+    int sum = num1 + num2;
+    cout << sum << endl;
+}
